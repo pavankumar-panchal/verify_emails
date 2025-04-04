5e6ebe05baeef853a338c8a49ad4a8724ca69d9d
pavankumar-panchal <panchalpavan800@gmail.com>
2025-04-04 17:58:10 +0530

    update

--- db.php
+++ db.php
@@ -1,10 +1,17 @@
 <?php
 
-$servername = "localhost";
-$username = "root";  // Change if needed
-$password = "";      // Change if needed
+$servername = "127.0.0.1";  // Change from "localhost" to "127.0.0.1"
+$username = "root";
+$password = "";
 $dbname = "email_id";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}
 
 ?>
